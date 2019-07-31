b9e8c98afc148ada2e04b2fee8e81254df77f08c
omar <omar_2124@hotmail.com>
2019-07-30 20:07:20 -0500

    Matching with my personal computer

---
+++ src/JavaBasics/Methods.java
@@ -0,0 +1,11 @@
+package JavaBasics;
+
+public class Methods {
+
+	public String ValidateHeader()
+	{
+		System.out.println("Headers links validated");
+		return "Test Case Pass";
+	}
+
+}
---
+++ src/JavaBasics/Parent.java
@@ -0,0 +1,13 @@
+package JavaBasics;
+
+public class Parent {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("I navigated to home page");
+		Methods m = new Methods();
+		System.out.println(m.ValidateHeader());
+	}
+
+}
